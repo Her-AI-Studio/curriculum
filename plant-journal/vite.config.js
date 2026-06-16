2168ea37500ab45953b582753519b25276d4d701
Jen Looper <jen.looper@gmail.com>
2026-06-16 18:30:25 -0400

    moving plant journal

--- plant-journal/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: '.',
-  server: { port: 5174, open: true },
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
-  },
-})
